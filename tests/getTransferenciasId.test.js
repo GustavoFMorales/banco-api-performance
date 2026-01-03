1d091205fc29bfa4142f74becc2758b9747d5272
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2026-01-03 13:51:23 -0300

    Adicionando teste de perfomance para o get de transferencia com um id especifico

---
+++ tests/getTransferenciasId.test.js
@@ -0,0 +1,29 @@
+import http from 'k6/http';
+import {sleep, check} from 'k6';
+import {obterTokenLogin} from '../helpers/autenticacao.js';
+import { pegarBaseUrl } from "../utils/variaveis.js";
+
+export const options = {
+    stages:[
+        {duration: '10s', target: 10},
+        {duration: '10s', target: 0}
+
+    ], thresholds: {
+        http_req_duration: ['p(95)<3000', 'p(90)< 3000', 'max<5000', 'avg<3000'],
+        http_req_failed: ['rate<0.01']
+    }
+}
+
+export default function () {
+    const token = obterTokenLogin();
+    const res = http.get(`${pegarBaseUrl()}/transferencias/1`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    check(res, {
+        "Validando que o status é 200": (r) => r.status === 200
+    });
+    sleep(1);
+}
